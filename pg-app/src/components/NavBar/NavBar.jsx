1cfbd830c51eaecafc4811f007476e01c407a8b0
phenom21 <aggarwalvinayak08@gmail.com>
2023-05-16 23:36:44 +0530

    added authentication

--- pg-app/src/components/NavBar/NavBar.jsx
+++ pg-app/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React,{useContext} from "react";
 import {Link} from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
+import Logout from "../Auth/Logout";
+
 let NavBar=()=>{
+    const { loggedIn } = useContext(AuthContext);
     return(
     <>
     <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
@@ -8,6 +12,9 @@ let NavBar=()=>{
         <Link to={"/"} className="navbar-brand text-info">
             Asset <span className="text-primary">Tracking</span>
         </Link>
+        {loggedIn == false && (<><Link to="/Register">Register</Link></>)}
+      {loggedIn == false && (<><Link to="/Login">Login</Link></>)}
+      {loggedIn == true && (<><Logout/></>)}
         </div>
     </nav>
     </>
--- pg-app/src/index.js
+++ pg-app/src/index.js
@@ -6,12 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import {BrowserRouter} from "react-router-dom"
+import { AuthContextProvider } from './context/AuthContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
